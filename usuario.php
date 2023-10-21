401a43c41eec259d3dda9fb92f90354b95297dae
Leonardo Gomes <leogomes.pereira03@gmail.com>
2023-10-21 12:14:09 -0300

    feat: Filtros usuario por username e id

--- usuario.php
+++ usuario.php
@@ -5,7 +5,26 @@ require 'settings.php';
 switch ($_SERVER['REQUEST_METHOD']) {
   case 'GET': {
       // Consulta SQL para recuperar dados da tabela tbusuario e, se freelancerusuario for 1, fazer um JOIN com tbfreelancerservico
+      $idusuario = isset($_GET['idusuario']);
       $username = isset($_GET['username']);
+
+      if (strlen($idusuario) > 0) {
+        $sql = "SELECT * from tbusuario where idusuario = '$_GET[idusuario]'";
+
+        // Executa a consulta SQL
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+          $response = $result->fetch_assoc();
+
+          // Retorna os resultados como JSON
+          echo json_encode($response);
+        } else {
+          // Não foram encontrados registros correspondentes
+          echo json_encode(array("message" => "Nenhum registro encontrado."));
+        }
+        return;
+      }
 
       if (strlen($username) > 0) {
         $sql = "SELECT * from tbusuario where username = '$_GET[username]'";
@@ -22,27 +41,29 @@ switch ($_SERVER['REQUEST_METHOD']) {
           // Não foram encontrados registros correspondentes
           echo json_encode(array("message" => "Nenhum registro encontrado."));
         }
-      } else {
-        $sql = "SELECT * from tbusuario";
-
-        // Executa a consulta SQL
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-          $response = array();
-
-          while ($row = $result->fetch_assoc()) {
-            // Para cada linha retornada, adicione-a à resposta
-            $response[] = $row;
-          }
-
-          // Retorna os resultados como JSON
-          echo json_encode($response);
-        } else {
-          // Não foram encontrados registros correspondentes
-          echo json_encode(array("message" => "Nenhum registro encontrado."));
-        }
+        return;
       }
+
+      $sql = "SELECT * from tbusuario";
+
+      // Executa a consulta SQL
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+        $response = array();
+
+        while ($row = $result->fetch_assoc()) {
+          // Para cada linha retornada, adicione-a à resposta
+          $response[] = $row;
+        }
+
+        // Retorna os resultados como JSON
+        echo json_encode($response);
+      } else {
+        // Não foram encontrados registros correspondentes
+        echo json_encode(array("message" => "Nenhum registro encontrado."));
+      }
+
     }
     break;
   case 'POST': {
@@ -116,6 +137,10 @@ switch ($_SERVER['REQUEST_METHOD']) {
       // Recebe os dados JSON da solicitação POST
       $data = json_decode(file_get_contents("php://input"));
 
+      $_SERVER['REQUEST_URI_PATH'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+      $pathSegments = explode('/', $_SERVER['REQUEST_URI_PATH']);
+      $idusuario = $pathSegments[count($pathSegments) - 1];
+
       // Verifica se os dados estão presentes e são válidos
       if (
         isset($data->nomeusuario) &&
@@ -125,12 +150,10 @@ switch ($_SERVER['REQUEST_METHOD']) {
         isset($data->generousuario) &&
         isset($data->emailusuario) &&
         isset($data->cpfusuario) &&
-        isset($data->senhausuario) &&
         isset($data->statususuario) &&
         isset($data->freelancerusuario)
       ) {
         // Dados do usuário recebidos
-        $idusuario = substr($_SERVER["PATH_INFO"], 1);
         $nomeusuario = $data->nomeusuario;
         $username = $data->username;
         $datanascusuario = $data->datanascusuario;
@@ -138,7 +161,6 @@ switch ($_SERVER['REQUEST_METHOD']) {
         $generousuario = $data->generousuario;
         $emailusuario = $data->emailusuario;
         $cpfusuario = $data->cpfusuario;
-        $senhausuario = $data->senhausuario;
         $statususuario = $data->statususuario;
         $freelancerusuario = $data->freelancerusuario;
 
@@ -155,7 +177,6 @@ switch ($_SERVER['REQUEST_METHOD']) {
                         generousuario = '$generousuario', 
                         emailusuario = '$emailusuario', 
                         cpfusuario = '$cpfusuario',
-                        senhausuario = '$senhausuario',
                         freelancerusuario= '$freelancerusuario',
                         statususuario = '$statususuario',
                         fotoperfilusuario = '$fotoperfilusuario',
